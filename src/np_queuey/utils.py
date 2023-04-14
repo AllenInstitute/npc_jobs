5500d232ce41d284ba1db686ebfc3095677818e1
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-04-13 23:33:57 -0700

    Init

--- src/np_queuey/utils.py
+++ src/np_queuey/utils.py
@@ -1,8 +1,5 @@
-try:
-    import np_config
-except:
-    DEFAULT_HUEY_SQLITE_DB_PATH = '/tmp/np_queuey_huey.db'
-else:
-    DEFAULT_HUEY_SQLITE_DB_PATH: str = np_config.fetch(
-        '/projects/np_queuey/config'
-    )['shared_huey_sqlite_db_path']
+import np_config
+
+DEFAULT_HUEY_SQLITE_DB_PATH: str = np_config.fetch(
+    '/projects/np_queuey/config'
+)['shared_huey_sqlite_db_path']
