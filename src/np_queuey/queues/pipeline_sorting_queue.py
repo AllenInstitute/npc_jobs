fa722287a779431742af743306863ee9b23a9201
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-05-06 21:04:58 -0700

    Fix dataclass subclass - must have dataclass decorator

--- src/np_queuey/queues/pipeline_sorting_queue.py
+++ src/np_queuey/queues/pipeline_sorting_queue.py
@@ -1,6 +1,9 @@
+import dataclasses
+
 from np_queuey.queues.sqlite_isilon_queue import SqliteJobQueue
 from np_queuey.utils import JobDataclass
 
+@dataclasses.dataclass
 class SortingJob(JobDataclass):
     probes: str = 'ABCDEF'
     
