d5a88c45ee92a39d9796611985a4e1ae08c1ada4
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-05-11 16:28:41 -0700

    Make Queues available at root

--- src/np_jobs/queues/sqlite_isilon/__init__.py
+++ src/np_jobs/queues/sqlite_isilon/__init__.py
@@ -4,3 +4,4 @@ from np_jobs.queues.sqlite_isilon.pipeline_qc_queue import *
 from np_jobs.queues.sqlite_isilon.pipeline_sorting_queue import *
 from np_jobs.queues.sqlite_isilon.codeocean_upload_queue import *
 from np_jobs.queues.sqlite_isilon.datajoint_upload_queue import *
+from . import dynamicrouting_behavior_session_mtrain_upload
